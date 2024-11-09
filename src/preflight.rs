d4a96734f5331fd1eead1a1b4b0774ce143a1955
Craig McLure <craig@mclure.net>
2024-11-09 12:29:38 +0100

    Fixed issues with PreFlight

--- src/preflight.rs
+++ src/preflight.rs
@@ -1,61 +1,43 @@
 use crate::{Message, PageMessages};
 use std::thread::sleep;
 use std::time::Duration;
+use sysinfo::{ProcessRefreshKind, RefreshKind, System, UpdateKind};
 use tokio::sync::mpsc::UnboundedSender;
 
-#[allow(dead_code)]
+
 const APP: &str = "GoXLR App.exe";
-#[allow(dead_code)]
 const BETA: &str = "GoXLR Beta App.exe";
-#[allow(dead_code)]
 const UTIL: &str = "goxlr-daemon.exe";
+const UTIL_LINUX: &str = "goxlr-daemon";
 
 pub fn status_check(sender: UnboundedSender<Message>) {
     println!("Starting Task Checker..");
-    let mut system = None;
 
-    #[cfg(target_family = "unix")]
-    {
-        use sysinfo::{ProcessRefreshKind, RefreshKind, System, UpdateKind};
-        let refresh_kind = RefreshKind::new()
-            .with_processes(ProcessRefreshKind::new().with_user(UpdateKind::Always));
-        system.replace(System::new_with_specifics(refresh_kind));
-    }
+    let kind = ProcessRefreshKind::new().with_user(UpdateKind::Always);
+    let refresh_kind = RefreshKind::new().with_processes(kind);
+    let mut system = System::new_with_specifics(refresh_kind);
 
     loop {
         let mut app_running = false;
         let mut beta_running = false;
         let mut utility_running = false;
 
-        #[cfg(target_os = "windows")]
-        {
-            unsafe {
-                let tasks = tasklist::Tasklist::new();
 
-                tasks.for_each(|task| {
-                    if task.get_pname() == APP {
-                        app_running = true;
-                    }
-                    if task.get_pname() == BETA {
-                        beta_running = true;
-                    }
-                    if task.get_pname() == UTIL {
-                        utility_running = true;
-                    }
-                });
-            }
+        system.refresh_processes();
+        if system.processes_by_exact_name(APP).count() > 0 {
+            app_running = true;
         }
-        #[cfg(target_family = "unix")]
-        {
-            if let Some(system) = &mut system {
-                system.refresh_processes();
-                let count = system.processes_by_exact_name("goxlr-daemon").count();
-                if count > 0 {
-                    utility_running = true;
-                }
-                app_running = false;
-                beta_running = false;
-            }
+
+        if system.processes_by_exact_name(BETA).count() > 0 {
+            beta_running = true;
+        }
+
+        if system.processes_by_exact_name(UTIL).count() > 0 {
+            utility_running = true;
+        }
+
+        if system.processes_by_exact_name(UTIL_LINUX).count() > 0 {
+            utility_running = true;
         }
 
         // Fire off the message..
